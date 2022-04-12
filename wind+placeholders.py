52c15e33cb9b52750cd78b28ded4d6c501ac7a90
MorganFlesland <103478299+MorganFlesland@users.noreply.github.com>
2022-04-11 22:36:49 -0700

    Add files via upload

---
+++ wind+placeholders.py
@@ -0,0 +1,21 @@
+######################################################################
+# Program Filename: wind
+# Author: Morgan Flesland
+# Date: 11 april 2022
+# Description: This program will compute the amount of energy made
+# from wind turbines of different efficiencies and compare the energy
+# output to that of burning coal.
+# Input: Turbine efficiency and size and wind speed
+# Output: Energy used from turbines
+######################################################################
+#given
+speed=(value1)
+r=(value2)
+efficiency=(value3)
+#equations
+a=3.14159265*(r**2)
+Pmax=0.5*1.2*a*(speed**3)
+Pactual=Pmax*efficiency
+#answers
+print(Pactual)
+print(Pmax)
